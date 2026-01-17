372dd253950cf8b794ecd46a562416d48affc89d
Akeka_ <vladvaledol@gmail.com>
2026-01-17 23:55:53 +0300

    Player new sprite and idle animations

--- Assets/Scripts/Entity/Enemies/EnemyVisual.cs
+++ Assets/Scripts/Entity/Enemies/EnemyVisual.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class EnemyVisual : MonoBehaviour
 {
     private Animator animator;
     private NavMeshAgent agent;
+    private Vector2 lastDirection;
     
     private const string IS_ROAMING_ANIM_PARAM = "IsRoaming";
+    private const string XInput_ANIM_PARAM = "XInput";
+    private const string YInput_ANIM_PARAM = "YInput";
     
     private void Awake()
     {
@@ -16,8 +20,23 @@ public class EnemyVisual : MonoBehaviour
 
     private void Update()
     {
-        bool isMoving = agent.velocity.magnitude > 0.1f;
-        animator.SetBool(IS_ROAMING_ANIM_PARAM, isMoving);
+        Vector3 velocity = agent.velocity;
+        Vector2 normalizedVelocity = velocity.normalized;
+
+        if (normalizedVelocity != Vector2.zero)
+        {
+            lastDirection = normalizedVelocity;
+            
+            animator.SetBool(IS_ROAMING_ANIM_PARAM, true);
+            animator.SetFloat(XInput_ANIM_PARAM, velocity.x);
+            animator.SetFloat(YInput_ANIM_PARAM, velocity.y);
+        }
+        else
+        {
+            animator.SetBool(IS_ROAMING_ANIM_PARAM, false);
+            animator.SetFloat(XInput_ANIM_PARAM, lastDirection.x);
+            animator.SetFloat(YInput_ANIM_PARAM, lastDirection.y);
+        }
     }
     
 }
--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class PlayerVisual : MonoBehaviour
 {
-    private const string IS_RUNNING_ANIM_PARAM = "IsRunning";
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Vector2 lastDirection;
+    
+    private const string IS_RUNNING_ANIM_PARAM = "IsRunning";
+    private const string XInput_ANIM_PARAM = "XInput";
+    private const string YInput_ANIM_PARAM = "YInput";
 
     private void Awake()
     {
@@ -14,8 +18,29 @@ public class PlayerVisual : MonoBehaviour
 
     private void Update()
     {
-        animator.SetBool(IS_RUNNING_ANIM_PARAM, Player.Instance.IsRunning());
+        UpdateMovementAnimation();
         AdjustPlayerFacingDirection();
+    }
+
+    private void UpdateMovementAnimation()
+    {
+        Vector2 movementInput = GameInput.Instance.GetInputVector();
+        animator.SetBool(IS_RUNNING_ANIM_PARAM, Player.Instance.IsRunning());
+
+        if (movementInput != Vector2.zero)
+        {
+            lastDirection = movementInput;
+            
+            animator.SetBool(IS_RUNNING_ANIM_PARAM, true);
+            animator.SetFloat(XInput_ANIM_PARAM, movementInput.x);
+            animator.SetFloat(YInput_ANIM_PARAM, movementInput.y);
+        }
+        else
+        {
+            animator.SetBool(IS_RUNNING_ANIM_PARAM, false);
+            animator.SetFloat(XInput_ANIM_PARAM, lastDirection.x);
+            animator.SetFloat(YInput_ANIM_PARAM, lastDirection.y);
+        }
     }
 
     private void AdjustPlayerFacingDirection()
